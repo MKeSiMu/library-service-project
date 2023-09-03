a744ec7da4d2add4bf6fc85385773dd88d361442
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-09-03 10:34:00 +0800

    Added IsAdminOrReadOnly permissions to Book API

---
+++ books/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    The request is authenticated as an admin, or is a read-only for all users (even those not authenticated).
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method
+                in SAFE_METHODS
+            )
+            or (request.user and request.user.is_staff)
+        )
