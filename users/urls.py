a0701443a071ec6e440d46565bd818a0665b1e9e
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-09-03 11:22:38 +0800

    Implemented User serializer & view

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from django.urls import path
+
+from users.views import UserCreateView, ManageUserView
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
 
 app_name = "users"
