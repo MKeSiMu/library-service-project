1da0dd16476916cb2784e8b1472354b983f8040b
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-09-08 21:56:05 +0800

    Created Payment model & added to admin

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = (("Pending", "PENDING"), ("Paid", "PAID"))
+    TYPE_CHOICES = (("Payment", "PAYMENT"), ("Fine", "FINE"))
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES)
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES)
+    borrowing_id = models.ForeignKey(
+        Borrowing, on_delete=models.PROTECT, related_name="payments"
+    )
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=100)
+    money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"Borrowing ID: {self.borrowing_id}; Status: {self.status}"
